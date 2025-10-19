df1332ded9f885179d3d5befb165b249ed0a1f5d
weberthwsm <weberthwsm@gmail.com>
2025-10-19 12:16:21 -0300

    Adicionado teste para validar login com credenciais inválidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,24 @@
 describe('Login', () => {
+ 
   it('Login com dados válidos devem permitir entrada no sistema', () => {
     cy.visit('http://localhost:4000')
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     //Asserts
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
 
+    it('Login com dados inválidos deve apresentar mensagem de erro', () => {
+    cy.visit('http://localhost:4000')
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').type('654321')
+    cy.contains('button', 'Entrar').click()
 
+    //Asserts
+    cy.get('.toast').should('have.text','Erro no login. Tente novamente.')
+    
+    //    cy.get('.toast').should('be.visible').contains('Erro no login. Tente novamente.') 
   })
 })
