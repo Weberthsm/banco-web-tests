35aeac88ef0bf4912e23125205a98889af1bc0ea
weberthwsm <weberthwsm@gmail.com>
2025-10-22 22:35:41 -0300

    Adicionado variaveis de ambiente através do package.json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,30 +1,31 @@
 describe('Login', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:4000')
+    cy.visit(Cypress.env('URL') || 'http://localhost:4000')
+    cy.screenshot('Tela de login inicial')
   })
 
   it('Login com dados válidos devem permitir entrada no sistema', () => {
 
-    cy.fixture('credenciais').then(credenciais => { 
+    cy.fixture('credenciais').then(credenciais => {
       cy.get('#username').click().type(credenciais.valida.usuario)
       cy.get('#senha').type(credenciais.valida.senha)
-    }) 
-
-    cy.screenshot('Após preencher dados válidos do login')
+      cy.screenshot('Após preencher dados válidos do login')
       cy.contains('button', 'Entrar').click()
       cy.screenshot('Após clicar no botão entrar')
-    //Asserts
-    cy.contains('h4', 'Realizar Transferência').should('be.visible')
+      //Asserts
+      cy.contains('h4', 'Realizar Transferência').should('be.visible')
+
+    })
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
 
-    cy.fixture('credenciais').then(credenciais => { 
+    cy.fixture('credenciais').then(credenciais => {
       cy.get('#username').click().type(credenciais.invalida.usuario)
       cy.get('#senha').type(credenciais.invalida.senha)
-    }) 
-      cy.contains('button', 'Entrar').click()
+    })
+    cy.contains('button', 'Entrar').click()
 
     //Asserts
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
