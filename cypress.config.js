297a68107d284b333d2385d9372d3a144598bc32
weberthwsm <weberthwsm@gmail.com>
2025-11-09 21:37:55 -0300

    Adicionando relatório html

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:'http://localhost:4000',
+    reporter: 'cypress-mochawesome-reporter',
     video: false,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
